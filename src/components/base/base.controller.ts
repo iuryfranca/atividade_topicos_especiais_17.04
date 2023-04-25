bcceb325c717ccc21cba32d45daea10b7c52b66d
Iury Franca <iurygfranca@gmail.com>
2023-04-25 15:35:49 -0400

    first commit

---
+++ src/components/base/base.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+
+export class BaseController {
+  // retorna somente status 200 e a mensagem de Api running (api rodando)
+  public index(req: Request, res: Response) {
+    res.status(200).json({ message: 'Api running....' })
+  }
+
+  // retorna informações sobre a api
+  public info(req: Request, res: Response) {
+    res.status(200).json({
+      name: 'API REST - Finan360',
+      mode: 'development',
+      version: '1.0.0',
+    })
+  }
+}
---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { BaseRoutes } from './base/base.routes'
+
+/**
+ * Init component routes
+ *
+ * @param {Router} router
+ * @param {string} prefix
+ * @returns {void}
+ */
+export function registerRoutes(router: Router, prefix: string = ''): void {
+  router.use(`${prefix}`, new BaseRoutes().routes())
+}
